81e535b54c44cabb533590e14bf20e0c3b062423
Gavin Carr <gavin@openfusion.com.au>
2013-09-27 12:57:39 +1000

    Add pattern maps, workaround Regexp::Assemble quantifier bugs.

--- lib/Business/CompanyDesignator.pm
+++ lib/Business/CompanyDesignator.pm
@@ -1,5 +1,8 @@
 package Business::CompanyDesignator;
 
+# Require perl 5.010 because the 'track' functionality of Regexp::Assemble
+# is unsafe for earlier versions.
+use 5.010;
 use Mouse;
 use FindBin qw($Bin);
 use YAML;
@@ -10,17 +13,29 @@ use Regexp::Assemble;
 our $VERSION = '0.01';
 
 has 'datafile' => ( is => 'ro', default => sub {
-  eval { module_file('Business::CompanyDesignator', 'company_designator.yml') }
-    # development/test version
-    || "$Bin/../share/company_designator.yml";
+  # Development/test version
+  my $local_datafile = "$Bin/../share/company_designator_dev.yml";
+  return $local_datafile if -f $local_datafile;
+  $local_datafile = "$Bin/../share/company_designator.yml";
+  return $local_datafile if -f $local_datafile;
+  # Installed version
+  return module_file('Business::CompanyDesignator', 'company_designator.yml');
 });
 
-has [ qw(data designator_regex) ]
+has [ qw(data assembler patterns designator_regex) ]
   => ( is => 'ro', lazy_build => 1 );
+
+has 'pattern_long_map'   => ( is => 'ro', isa => 'HashRef', default => sub { {} } );
+has 'pattern_string_map' => ( is => 'ro', isa => 'HashRef', default => sub { {} } );
 
 sub _build_data {
   my $self = shift;
   YAML::LoadFile($self->datafile);
+}
+
+sub _build_assembler {
+  my $self = shift;
+  Regexp::Assemble->new->track(1);
 }
 
 sub long_designators {
@@ -43,21 +58,80 @@ sub designators {
   sort $self->long_designators, $self->abbreviations;
 }
 
+# Convert a designator string into a pattern
+sub _string_to_pattern {
+  my ($self, $string) = @_;
+  my $pattern = $string;
+
+  # Treat all periods as optional
+  $pattern =~ s/\./\\.?/g;
+
+  # Record mapping in pattern_string_map
+  $self->pattern_string_map->{$pattern} = $string;
+
+  return $pattern;
+}
+
+sub _add_to_pattern_long_map {
+  my ($self, $pattern, $value) = @_;
+  $self->pattern_long_map->{ $pattern } ||= [];
+  push @{ $self->pattern_long_map->{ $pattern } }, $value;
+}
+
+# patterns is an ordered arrayref of patterns derived from designator strings
+# Collating also builds a pattern_long_map of pattern => long_designator, and
+# a pattern_string_map of pattern => source string
+sub _build_patterns {
+  my $self = shift;
+
+  # Build patterns list and patterns map
+  my (@pattern_long, @pattern_abbr);
+  while (my ($long_designator, $entry) = each %{ $self->data }) {
+    # Add long_designator patterns
+    push @pattern_long, $long_designator;
+    my $pattern = $self->_string_to_pattern($long_designator);
+#   push @pattern_long, $pattern;
+    $self->_add_to_pattern_long_map($pattern => $long_designator);
+
+    # Add all abbreviations
+    if (my $abbr_list = $entry->{abbr}) {
+      $abbr_list = [ $abbr_list ] if ! ref $abbr_list;
+      for my $abbr (@$abbr_list) {
+        push @pattern_abbr, $abbr;
+        $pattern = $self->_string_to_pattern($abbr);
+#       push @pattern_abbr, $pattern;
+        $self->_add_to_pattern_long_map($pattern => $long_designator);
+      }
+    }
+  }
+
+  # FIXME: do we need to sort these patterns??
+  # sort({ length $a <=> length $b } @pattern_long)
+  # sort({ length $a <=> length $b } @pattern_abbr)
+  return [ @pattern_long, @pattern_abbr ];
+}
+
+# Assemble designator regex
 sub _build_designator_regex {
   my $self = shift;
 
-  my @des;
-  # For abbreviations, make all periods optional
-  push @des, map { s/\./\.?/g; $_ } sort { length $b <=> length $a } $self->abbreviations;
-  # Add long_designators verbatim
-  push @des, sort { length $b <=> length $a } $self->long_designators;
+  my @patterns = @{ $self->patterns };
 
-  # Assemble regex
-  my $ra = Regexp::Assemble->new;
-  $ra->add(@des);
-  return $ra->re;
+  # FIXME: RA->add() doesn't work here because of known quantifier-escaping bugs:
+  # https://rt.cpan.org/Public/Bug/Display.html?id=50228
+  # https://rt.cpan.org/Public/Bug/Display.html?id=74449
+  # $self->assembler->add(@patterns);
+  # Workaround by lexing and using insert()
+  for my $string (@patterns) {
+    $self->assembler->insert(map { /\./ ? '\\.?' : $_ } split //, $string);
+  }
+
+  return $self->assembler->re;
 }
 
+# Strip trailing designator from $company_name.
+# In scalar context returns the stripped name; in list context returns
+# a list containing ($stripped_name, $designator).
 sub strip_designator {
   my $self = shift;
   my $company_name = shift;
@@ -65,10 +139,17 @@ sub strip_designator {
   my $re = $self->designator_regex;
 
   if ($company_name =~ m/(.*?)\s*($re)\s*$/) {
-    return wantarray ? ($1, $2) : $1;
+    my $matched = $self->assembler->source($^R);
+    return wantarray ? ($1, $2, $self->pattern_string_map->{$matched}) : $1;
+#   my $long_designators = $self->pattern_long_map->{ $pattern_matched };
+#   my $match = Business::CompanyDesignator::Match->new(
+#     pattern           => $pattern_matched,
+#     long_designators  => $long_designators,
+#     languages         => [ map { $self->data->{$_}->{lang} } @$long_designators ],
+#   );
   }
   else {
-    return wantarray ? ($company_name, undef) : $company_name;
+    return wantarray ? ($company_name) : $company_name;
   }
 }
 
@@ -89,7 +170,7 @@ company designators appended to company names
   $bcd = Business::CompanyDesignator->new;
   $bcd = Business::CompanyDesignator->new(datafile => '/path/to/company_designator.yml');
 
-
+  # Strip any trailing designator from $company_name
   $stripped_name = $bcd->strip_designator($company_name);
   ($stripped_name, $designator) = $bcd->strip_designator($company_name);
 
--- t/10_strip_designator.t
+++ t/10_strip_designator.t
@@ -1,5 +1,7 @@
 #!perl
 
+use 5.010;
+use strict;
 use Test::More;
 use Business::CompanyDesignator;
 use Data::Dump qw(dd pp dump);
@@ -7,20 +9,27 @@ use Data::Dump qw(dd pp dump);
 binmode(\*STDOUT, ':utf8');
 
 my @testdata = (
-  [ 'News Corporation'          => 'News',              'Corporation' ],
-  [ 'Open Fusion Pty. Ltd.'     => 'Open Fusion',       'Pty. Ltd.' ],
+  [ 'News Corporation'          => 'News',                      'Corporation',  'Corporation' ],
+  [ 'Open Fusion Pty Ltd.'      => 'Open Fusion',               'Pty Ltd.',     'Pty. Ltd.' ],
+  # Check we are only matching literal dots
+  [ 'Open Fusion Pty1 Ltd2'     => 'Open Fusion Pty1 Ltd2',     undef,          undef  ],
 );
 
-my ($bcd, $data, $strip, $des);
+my ($bcd, $data, $strip, $des, $match);
 
 ok($bcd = Business::CompanyDesignator->new, 'constructor ok');
 ok($data = $bcd->data, 'data method ok');
 #dd $data;
+#say $bcd->designator_regex;
 for my $t (@testdata) {
+  my ($company_name, $stripped_name, $designator, $matching) = @$t;
+
   # List context
-  ($strip, $des) = $bcd->strip_designator($t->[0]);
-  is($strip, $t->[1], "$t->[0]: stripped name ok");
-  is($des,   $t->[2], "$t->[0]: designator ok");
+  ($strip, $des, $match) = $bcd->strip_designator($company_name);
+
+  is($strip, $stripped_name, "$company_name: stripped name ok: $strip");
+  is($des,   $designator, "$company_name designator ok: " . ($des // 'undef'));
+  is($match, $matching, "$company_name match ok: " . ($match // 'undef'));
 }
 
 done_testing;
