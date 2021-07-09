dbd955e7c99a0610a637c1a1ae379d863ba6d3a3
Gavin Carr <gavin@openfusion.com.au>
2021-07-09 17:02:58 +1000

    Bump VERSION to 0.18.

--- lib/Business/CompanyDesignator.pm
+++ lib/Business/CompanyDesignator.pm
@@ -17,7 +17,7 @@ use Carp;
 use Business::CompanyDesignator::Record;
 use Business::CompanyDesignator::SplitResult;
 
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 # Hardcode the set of languages that we treat as 'continuous'
 # i.e. their non-ascii designators don't require a word break
@@ -407,7 +407,7 @@ company designators appended to company names
 
 =head1 VERSION
 
-Version: 0.17.
+Version: 0.18.
 
 This module is considered a B<BETA> release. Interfaces may change and/or break
 without notice until the module reaches version 1.0.
